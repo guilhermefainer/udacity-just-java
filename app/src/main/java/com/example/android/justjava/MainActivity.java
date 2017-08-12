3c9cb7fab50f50c7f353f5b4c31c4f8b6f6c94df
Guilherme Fainer Dalben <guilherme.fainer@gmail.com>
2017-08-12 16:16:53 -0300

    First lesson of Android Apps ud837

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.android.justjava;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static java.text.NumberFormat.*;
+
+public class MainActivity extends Activity {
+
+    int quantity = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(quantity);
+        displayPrice(quantity*5);
+    }
+
+    public void increment(View view){
+        quantity++;
+        display(quantity);
+    }
+
+    public void decrement(View view){
+        quantity--;
+        display(quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_View);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(getCurrencyInstance().format(number));
+    }
+}
